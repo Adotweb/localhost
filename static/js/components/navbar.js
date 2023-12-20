fc0f084e499b60744853cb47c40024f1e32a6865
Alim <alim.weber@outlook.com>
2023-12-20 15:32:10 +0100

    header into componentfile

---
+++ static/js/components/navbar.js
@@ -0,0 +1,38 @@
+function checkAccount(){
+	const user = $get("user")
+
+	if(user){
+		let accountref = document.getElementById("accountref")
+
+		accountref.innerHTML = user.name
+	}else {
+
+	}
+}
+
+
+
+const navBarText = `	<div class="sticky top-0 left-0 z-10 flex justify-between p-4 text-xl backdrop-blur-md font-bold">
+		<div class="right flex justify-space gap-4">
+			<a href="./server">Server</a>
+			<a href="./client">Client</a>
+			<a href="./documentation">Docs</a>
+		</div>
+
+
+		<div class="right flex justify-space gap-4">
+			<a href="./about">About</a>
+			<a href="./team">Ω</a>
+			<a id="accountref" href="./account">Sign up</a>
+		</div>
+	</div>
+
+`
+
+
+let navBar = document.querySelector(".navBar");
+
+navBar.outerHTML = navBarText
+
+
+checkAccount()
--- static/js/data.js
+++ static/js/data.js
@@ -1,66 +1,7 @@
-function $store(key, object){
-		
-	localStorage.setItem(key, JSON.stringify(object))
+function $set(key, object){
+	localStorage.setItem(key, JSON.stringify(key))
 }
-
 
 function $get(key){
 	return JSON.parse(localStorage.getItem(key))
-}
-
-
-function $deleteKey(key){
-	localStorage.removeItem(key)
-}
-
-
-
-async function redirectToProduct(product){
-
-	let session = $get("user-session");
-
-	fetch("/account/verify-session", {
-		method:"POST",
-		headers:{
-			"Content-Type":"application/json",
-		},
-		body:JSON.stringify({session})
-	}).then(res => res.json())
-	
-		.then(res => {
-
-
-			if(res.success){
-
-				$store("user", res.user)
-
-
-				if(product === "lan"){
-					window.location.href = "/account"; 
-
-				}if(product === "server"){
-					window.location.href = "/checkout";
-				}if(product === "aws"){
-					window.location.href = "/contact"
-				}
-
-			} else{
-				window.location.href = "/account"	
-			}
-		})
-
-
-}
-
-function navbarAccount(){
-	
-	document.addEventListener("DOMContentLoaded", () => {
-		let accountLink = document.getElementById("account") 
-
-
-		accountLink.innerHTML = $get("user-session") ? "Account" : "Sign up"
-
-	
-	}) 
-
 }
